dfc95e3e1acf9fb904f47f92a06b9e3c0aca6cb0
Mohamed Falilou Fall <mff.falilou.fall@gmail.com>
2025-12-26 20:07:01 +0000

    Update 08_Scenarios.py

--- pages/08_Scenarios.py
+++ pages/08_Scenarios.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+from plotly import express as px
 
 def run():
     st.title("Scénarios financiers & analyse de sensibilité")
